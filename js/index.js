3740e7cea0962c68457e87b7835215ae8caf96fb
gzifoto-spec <gzifoto@gmail.com>
2025-12-15 12:53:55 +0100

    fix: added const safeJoin that returns 'No one' for emptyfields

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://api.disneyapi.dev/character?page=1&limit=100';
+const requestURL = 'https://api.disneyapi.dev/character?page=1&pageSize=100';
 
 async function fetchCharactersJson() {
     try {
@@ -13,13 +13,17 @@ async function fetchCharactersJson() {
     }
 }
 
-function createCharacterCard({ name, image }) {
+function createCharacterCard({ name, imageUrl, films, tvShows, videoGames }) {
+    const safeJoin = arr => (Array.isArray(arr) && arr.length > 0) ? arr.join(', ') : 'No one';
+
     return `
         <div class="card" style="width: 18rem;" id="cardDisney">
-            <img src="${image}" class="card-img-top" alt="Imagen del personaje" style="max-height: 350px; object-fit: contain; width: 100%;">
+            <img src="${imageUrl}" class="card-img-top" alt="Imagen del personaje" style="max-height: 350px; object-fit: contain; width: 100%;">
             <div class="card-body">
-                <h5 class="card-title">${name}</h5>
-                
+                <h5 class="card-title">Name: ${name}</h5>
+                <h6 class="card-films">Film: ${safeJoin(films)}</h6>
+                <h6 class="card-shows">TV Shows: ${safeJoin(tvShows)}</h6>
+                <h6 class="card-games">Video Games: ${safeJoin(videoGames)}</h6>
             </div>
         </div>
     `;
